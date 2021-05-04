99443f751c390e01361293f0858e0f5015e00c8b
abilash222 <abilash_222@yahoo.co.in>
2021-05-04 10:41:45 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -50,4 +50,50 @@ resource "aws_instance" "ubuntu" {
   vpc_security_group_ids = [
     aws_security_group.ubuntu.id
   ]
+    user_data     = <<EOT
+#cloud-config
+# update apt on boot
+package_update: true
+# install nginx
+packages:
+- nginx
+write_files:
+- content: |
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>StackPath - Amazon Web Services Instance</title>
+      <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+      <style>
+        html, body {
+          background: #000;
+          height: 100%;
+          width: 100%;
+          padding: 0;
+          margin: 0;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          flex-flow: column;
+        }
+        img { width: 250px; }
+        svg { padding: 0 40px; }
+        p {
+          color: #fff;
+          font-family: 'Courier New', Courier, monospace;
+          text-align: center;
+          padding: 10px 30px;
+        }
+      </style>
+    </head>
+    <body>
+      <img src="https://www.stackpath.com/content/images/logo-and-branding/stackpath-logo-standard-screen.svg">
+      <p>This request was proxied from <strong>Amazon Web Services</strong></p>
+    </body>
+    </html>
+  path: /usr/share/app/index.html
+  permissions: '0644'
+runcmd:
+- cp /usr/share/app/index.html /usr/share/nginx/html/index.html
+EOT
 }
